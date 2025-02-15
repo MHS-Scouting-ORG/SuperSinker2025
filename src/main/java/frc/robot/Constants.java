1e865c70aa69094549b075a44cb629813d98740e
DylanJakeComilang <dylanjakecomilang@gmail.com>
2025-02-15 10:09:45 -1000

    Replaced swerve code with CTRE generated code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,89 +4,9 @@ import com.ctre.phoenix6.signals.InvertedValue;
 
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-import frc.robot.subsystems.SwerveModuleConstants;
+
 
 public class Constants {
-  public static class SwerveConstants {  
-
-        public static final double kWheelDiameter = 4 * 2.5 / 100;
-        public static final double kTrackWidth = 0.635;
-        public static final double kWheelBase = 0.635;
-
-        public static final double kGearRatio = 8.14 / 1;
-
-        public static final double kVoltage = 7.2;
-
-        public static final SwerveDriveKinematics kKinematics = new SwerveDriveKinematics(
-           //FL
-           new Translation2d(kWheelBase / 2, kTrackWidth / 2),
-           //BL
-           new Translation2d(-kWheelBase / 2, kTrackWidth / 2),
-           //FR
-           new Translation2d(kWheelBase / 2, -kTrackWidth / 2),
-           //BR
-           new Translation2d(-kWheelBase / 2, -kTrackWidth / 2)
-        );
-
-    public static class FrontLeft{
-        public static final int kDriveID = 1;
-        public static final int kTurningID = 5;
-        public static final int kCANID = 9;
-        public static final InvertedValue kDriveInverted = InvertedValue.Clockwise_Positive; 
-        public static final InvertedValue kTurningInverted = InvertedValue.CounterClockwise_Positive; 
-        public static final double kEncOffset = 0;
-
-        public static final SwerveModuleConstants constants = new SwerveModuleConstants(kDriveID, kTurningID, kCANID, kDriveInverted, kTurningInverted, kEncOffset);
-    }
-
-    public static class BackLeft{
-      public static final int kDriveID = 2;
-      public static final int kTurningID = 6;
-      public static final int kCANID = 10;
-      public static final InvertedValue kDriveInverted = InvertedValue.Clockwise_Positive; 
-      public static final InvertedValue kTurningInverted = InvertedValue.CounterClockwise_Positive;
-      public static final double kEncOffset = 0;
-
-      public static final SwerveModuleConstants constants = new SwerveModuleConstants(kDriveID, kTurningID, kCANID, kDriveInverted, kTurningInverted, kEncOffset);
-    }
-
-    public static class FrontRight{
-      public static final int kDriveID = 4;
-      public static final int kTurningID = 8;
-      public static final int kCANID = 12;
-      public static final InvertedValue kDriveInverted = InvertedValue.Clockwise_Positive; 
-      public static final InvertedValue kTurningInverted = InvertedValue.CounterClockwise_Positive; 
-      public static final double kEncOffset = 0;
-
-      public static final SwerveModuleConstants constants = new SwerveModuleConstants(kDriveID, kTurningID, kCANID, kDriveInverted, kTurningInverted, kEncOffset);
-    }
-
-    public static class BackRight{
-      public static final int kDriveID = 3;
-      public static final int kTurningID = 7;
-      public static final int kCANID = 11;
-      public static final InvertedValue kDriveInverted = InvertedValue.Clockwise_Positive; 
-      public static final InvertedValue kTurningInverted = InvertedValue.CounterClockwise_Positive; 
-      public static final double kEncOffset = 0;
-
-      public static final SwerveModuleConstants constants = new SwerveModuleConstants(kDriveID, kTurningID, kCANID, kDriveInverted, kTurningInverted, kEncOffset);
-    }
-
-    public static final double kPositionConversionFactor = kGearRatio * Math.PI * kWheelDiameter;
-    public static final double kVelocityConversionFactor = kPositionConversionFactor / 60;
-
-    public static final double turningKP = 0.0;
-    public static final double turningKI = 0.0;
-    public static final double turningKD = 0.0;
-
-
-    public static final double drivingKP = 0.0;
-    public static final double drivingKI = 0.0;
-    public static final double drivingKD = 0.0;
-
-    public static final double kMaxSpeed = 3.6576;
-    }
-      
     public static class AlgaeIntakeConstants{
       //Motor ID's
       public static final int INTAKEID = 15;
@@ -159,7 +79,6 @@ public class Constants {
     public static final int kDriverControllerPort = 0;
    
   }
-
 
 }
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,7 +2,6 @@ package frc.robot;
 
 import frc.robot.commands.CoralDepolyerCommand;
 import frc.robot.commands.CoralIntakeCommand;
-import frc.robot.commands.DriveCommand;
 import frc.robot.commands.AlgaeIntakeCmds.IntakeCmd;
 import frc.robot.commands.AlgaeIntakeCmds.OuttakeCmd;
 import frc.robot.commands.AlgaePivotCmds.StoragePositionCmd;
@@ -19,13 +18,45 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.subsystems.AlgaeIntakeSubsystem;
 import frc.robot.subsystems.CoralIntakeSubsystem;
 import frc.robot.subsystems.ElevatorSubsystem;
-import frc.robot.subsystems.SwerveDriveSubsystem;
+
+import static edu.wpi.first.units.Units.*;
+
+import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.swerve.SwerveRequest;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
+
+import frc.robot.TunerConstants;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
 
 public class RobotContainer {
+  private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
+  private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
+
+  /* Setting up bindings for necessary control of the swerve drive platform */
+  private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
+  .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
+  .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
+private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
+private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
+
+private final Telemetry logger = new Telemetry(MaxSpeed);
+
+private final CommandXboxController joystick = new CommandXboxController(0);
+
+public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
+
 
   private final SendableChooser<Command> autoChooser;
   
-  SwerveDriveSubsystem s_Subsystem = new SwerveDriveSubsystem();
+  
   private final CoralIntakeSubsystem coralIntakeSub = new CoralIntakeSubsystem();
   public final AlgaeIntakeSubsystem algaeIntakeSubsystem = new AlgaeIntakeSubsystem();
   public final ElevatorSubsystem elevatorSubsystem = new ElevatorSubsystem();
@@ -36,16 +67,41 @@ public class RobotContainer {
   public RobotContainer() {
     autoChooser = AutoBuilder.buildAutoChooser();
     SmartDashboard.putData("Auto Chooser", autoChooser);
-
-    s_Subsystem.setDefaultCommand(new DriveCommand(s_Subsystem, () -> xboxDriver.getLeftY() * 0.3, () -> xboxDriver.getLeftX() * 0.3, () -> xboxDriver.getRightX() * 0.6, false));
     configureBindings();
   }
 
   private void configureBindings() {
 
     /// SWERVE 
-    
-    new JoystickButton(xboxDriver, XboxController.Button.kA.value).onTrue(new InstantCommand(s_Subsystem::resetPigeon2));
+    // Note that X is defined as forward according to WPILib convention,
+        // and Y is defined as to the left according to WPILib convention.
+        drivetrain.setDefaultCommand(
+            // Drivetrain will execute this command periodically
+            drivetrain.applyRequest(() ->
+                drive.withVelocityX(-joystick.getLeftY() * MaxSpeed * 0.4) // Drive forward with negative Y (forward)
+                    .withVelocityY(-joystick.getLeftX() * MaxSpeed * 0.4) // Drive left with negative X (left)
+                    .withRotationalRate(-joystick.getRightX() * MaxAngularRate * 0.6) // Drive counterclockwise with negative X (left)
+            )
+        );
+
+        joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
+        joystick.b().whileTrue(drivetrain.applyRequest(() ->
+            point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))
+        ));
+
+        // Run SysId routines when holding back/start and X/Y.
+        // Note that each routine should be run exactly once in a single log.
+        joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
+        joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
+        joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
+        joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
+
+        // reset the field-centric heading on left bumper press
+        joystick.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
+
+        drivetrain.registerTelemetry(logger::telemeterize);
+
+        // new JoystickButton(joystick, XboxController.Button.kA.value).onTrue(new InstantCommand(drivetrain::))
 
     /// CORAL
 
